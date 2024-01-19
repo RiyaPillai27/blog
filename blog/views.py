43170b3ddcbb896e7e63e8239c83a662af972e57
RiyaPillai27 <riapillai27@gmail.com>
2024-01-19 20:09:26 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post, Category
+
+# Create your views here.
+def home(request):
+    # load all the post from db(10)
+    posts = Post.objects.all()[:11]
+    # print(posts)
+    cats = Category.objects.all()
+
+    data = {
+        'posts': posts,
+        'cats': cats
+    }
+    return render(request, 'home.html', data)
+
+def post(request, url):
+    #post = Post.objects.filter(url=url)
+    post = get_object_or_404(Post, url=url)
+    cats = Category.objects.all()
+    # print(post)
+    return render(request, 'posts.html', {'post': post, 'cats': cats})
+
+
+def category(request, url):
+    cat = Category.objects.get(url=url)
+    posts = Post.objects.filter(cat=cat)
+    return render(request, "category.html", {'cat': cat, 'posts': posts})
